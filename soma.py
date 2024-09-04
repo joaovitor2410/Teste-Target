76820a2d9d3b9fe8461e715bfcbd9d496d0653ca
joaovitor2410 <159330960+joaovitor2410@users.noreply.github.com>
2024-09-03 23:25:07 -0300

    Add files via upload

---
+++ soma.py
@@ -0,0 +1,16 @@
+#inicializando as variaveis
+indice = 13
+soma = 0
+k = 0
+
+#Controle de repetição
+while k < indice:
+    k = k + 1
+    soma = soma + k
+
+#mostrar o final da soma, acumulando os valores
+print(soma)
+
+#dessa forma meu resultado nesse codígo sera 91
+
+
